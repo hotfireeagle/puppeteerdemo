7893815b64c2e01535450f1b421217ef13ef21e6
hotfireeagle <hotfireeagle@163.com>
2021-05-30 17:17:10 +0800

    feat: 下载

--- demo1.js
+++ demo1.js
@@ -2,7 +2,11 @@
  * 功能描述：杀鸡用牛刀，爬取镖人漫画
  */
 const puppeteer = require("puppeteer")
-const cheerio = require('cheerio')
+const cheerio = require("cheerio")
+const path = require("path")
+const fs = require("fs")
+const axios = require("axios")
+const fsPromises = fs.promises
 
 let page
 const entry = "https://www.dmzj.com/info/biaoren.html" // 爬取入口
@@ -21,12 +25,10 @@ const sleep = (s = sleepTime, log=true) => {
  * 分析每章动画的入口地址
  * @param {*} htmlTxt : 入口页面的html
  */
-const analyzeChapters = async htmlTxt => {
+const analyzeChapters = htmlTxt => {
   console.log("[LOG]: 开始获取每章动画的入口地址")
   const $ = cheerio.load(htmlTxt)
-  const list = $("div.tab-content.tab-content-selected.zj_list_con > ul.list_con_li > li > a")
-  for (let idx = 0; idx < list.length; idx++) {
-    const ele = list[idx]
+  $("div.tab-content.tab-content-selected.zj_list_con > ul.list_con_li > li > a").each((idx, ele) => {
     const url = ele?.attribs?.href
     const title = ele?.attribs?.title || idx
     console.log("[LOG]:", idx, title, url)
@@ -34,7 +36,7 @@ const analyzeChapters = async htmlTxt => {
       const obj = { url, title }
       chapterList.unshift(obj)
     }
-  }
+  })
   console.log(`[LOG]: 获取每章动画的入口地址完成，共有${chapterList.length}章节`)
 }
 
@@ -42,18 +44,64 @@ const analyzeChapters = async htmlTxt => {
  * 分析每章动画有几页
  * @param {*} htmlTxt ： 每章动画的入口页面html
  */
-const analyChapterLength = async () => {
+const downloadChapter = async () => {
   for (let i = 0; i < chapterList.length; i++) {
     const obj = chapterList[i]
     const { title, url } = obj
-    console.log(`[LOG]: 开始分析${title}页数`)
+    const filePath = `镖人/${title}`
+    await mkdirHandler(filePath)
     await page.goto(url, { waitUntil: "domcontentloaded" })
     await sleep(500, false)
     const arr = await page.$$("#page_select > option")
-    const len = arr.length
-    obj.len = len
-    console.log(`[LOG]: 分析完毕，${title}共有${len}页`)
+    console.log(arr.length)
+    for (let idx = 0; idx < arr.length; idx++) {
+      const sel = `#page_select > option:nth-child(${idx+1})`
+      const imgUrl = await page.$eval(sel, ele => ele.value)
+      await downloadImg(filePath, idx+1, imgUrl)
+    }
   }
+}
+
+/**
+ * 创建文件夹
+ * @param {*} title 
+ * @returns 
+ */
+const mkdirHandler = async title => {
+  console.log(`[LOG]: 开始新建${title}文件夹`)
+  const baseSave = path.join("./", title)
+  let isDirExists = false
+  try {
+    const fsStatus = await fsPromises.stat(baseSave)
+    if (fsStatus.isDirectory()) {
+      isDirExists = true
+    }
+  } catch(_) {}
+  if (isDirExists) {
+    console.log(`[LOG]: ${title}文件夹已存在`)
+    return
+  }
+  await fsPromises.mkdir(baseSave)
+  console.log(`[LOG]: ${title}文件夹创建成功`)
+}
+
+/**
+ * 下载并保存单张图片
+ * @param {*} chapterPath ： 章节存储位置
+ * @param {*} i : 索引当作文件名
+ * @param {*} imgUrl : 图片链接
+ */
+const downloadImg = async (chapterPath, i, imgUrl) => {
+  console.log(`[LOG]: 开始下载${imgUrl}`)
+  const res = await axios({
+    method: "get",
+    url: imgUrl,
+    responseType: "stream",
+  })
+  const picDir = path.join("./", chapterPath, `${i}.jpg`)
+  const writer = fs.createWriteStream(picDir)
+  res.data.pipe(writer)
+  console.log(`[LOG]: 成功下载${imgUrl}`)
 }
 
 const main = async () => {
@@ -63,9 +111,8 @@ const main = async () => {
   const txt = await res.text()
 
   await analyzeChapters(txt)
-  await sleep()
-  await analyChapterLength()
-
+  await mkdirHandler("镖人")
+  await downloadChapter()
   await browser.close()
 }
 
